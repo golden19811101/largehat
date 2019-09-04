cab493a65bf748e4d6f8b2cf95b81ce9ece1fc18
pengbin001@163.com <pengbin001@163.com>
2019-09-04 15:39:40 +0800

    修改了一遍！

--- largehat-admin/src/main/java/com/largehat/admin/modules/quartz/config/JobRunner.java
+++ largehat-admin/src/main/java/com/largehat/admin/modules/quartz/config/JobRunner.java
@@ -3,6 +3,7 @@ package com.largehat.admin.modules.quartz.config;
 import com.largehat.admin.modules.quartz.domain.QuartzJob;
 import com.largehat.admin.modules.quartz.repository.QuartzJobRepository;
 import com.largehat.admin.modules.quartz.utils.QuartzManage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -15,6 +16,7 @@ import java.util.List;
  * @date 2019-01-07
  */
 @Component
+@Slf4j
 public class JobRunner implements ApplicationRunner {
 
     @Autowired
@@ -30,11 +32,11 @@ public class JobRunner implements ApplicationRunner {
      */
     @Override
     public void run(ApplicationArguments applicationArguments){
-        System.out.println("--------------------注入定时任务---------------------");
+        log.info("---------------------注入定时任务------------------------");
         List<QuartzJob> quartzJobs = quartzJobRepository.findByIsPauseIsFalse();
         quartzJobs.forEach(quartzJob -> {
             quartzManage.addJob(quartzJob);
         });
-        System.out.println("--------------------定时任务注入完成---------------------");
+        log.info("--------------------定时任务注入完成---------------------");
     }
 }
