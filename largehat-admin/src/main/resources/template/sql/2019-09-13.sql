d4a195ec7919f0c54d172520192a40a339e76f84
tomcat <1794909127@qq.com>
2019-09-13 19:38:13 +0800

    新增sql语句

---
+++ largehat-admin/src/main/resources/template/sql/2019-09-13.sql
@@ -0,0 +1,3 @@
+INSERT INTO `xinbo_im`.`sys_menu` (`create_time`, `i_frame`, `name`, `component`, `pid`, `sort`, `icon`, `path`) VALUES ('2019-09-12 16:09:55', b'0', '客户端管理', '', '0', '40', 'phone', 'sys_client');
+INSERT INTO `xinbo_im`.`sys_menu` (`create_time`, `i_frame`, `name`, `component`, `pid`, `sort`, `icon`, `path`) VALUES ('2019-09-12 20:27:11', b'0', 'OSS配置管理', 'system/oss/index', '44', '41', 'system', 'oss');
+INSERT INTO `xinbo_im`.`sys_dict` (`name`, `remark`) VALUES ('oss_status', 'oss配置状态');
